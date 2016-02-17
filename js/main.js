80316f6b9a47b6113b89e4654a72bc0c00551586
Serhiy <serhiyhulyi93@gmail.com>
2016-02-17 03:28:13 +0200

    almost ready

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// Global parameters
+window.params = {
+  widthFull: 750,
+  maxRowHeight: 0,
+  isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+  isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent)
+};
+/*
+#############################
+#   Main JS for ____________   #
+#############################
+*/
+
+jQuery(document).ready(function($) {
+
+
+/*-----------------------------------------------------------------*/  
+  $('.magnific').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+    modal: false,
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
+
+  $(document).on('click', '.popup-modal-dismiss', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
+
+  /*$('.services_grid').masonry({
+    // options
+    itemSelector: '.grid_box',
+  });*/
+
+});
+
+
+
+
