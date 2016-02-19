2a2c7c875a489637c15b381d8a36c47f7f15c9f6
Serhiy <serhiyhulyi93@gmail.com>
2016-02-20 00:27:56 +0200

    some edits

--- js/main.js
+++ js/main.js
@@ -13,9 +13,6 @@ window.params = {
 
 
 jQuery(document).ready(function($) {
-  if ( $('html').hasClass('no-flexbox') ) {
-    console.log('log');
-  }
 
 /*-----------------------------------------------------------------*/  
   $('.magnific').magnificPopup({
@@ -54,45 +51,6 @@ jQuery(document).ready(function($) {
     $('.mobile_menu').removeClass('active');
     $('body, html').removeClass('menu_active');
   });
-
-  $('.slider').slick({
-    fade: true,
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 5000
-  })
-
-
-
-  function googleMap_initialize() {
-      var lat = $('#map-canvas').attr('data-lat');
-      var long = $('#map-canvas').attr('data-long');
-      var mapCenterCoord = new google.maps.LatLng(lat, long);
-      var mapMarkerCoord = new google.maps.LatLng(lat, long);
-
-      var mapOptions = {
-        center: mapCenterCoord,
-        zoom: 16,
-        //draggable: false,
-        disableDefaultUI: false,
-        scrollwheel: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-
-      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-      var markerImage = new google.maps.MarkerImage('images/green-marker.svg');
-      var marker = new google.maps.Marker({
-        icon: markerImage,
-        position: mapMarkerCoord, 
-        map: map,
-        title:"ULTRA Fitness"
-      });
-      $(window).resize(function (){
-        map.setCenter(mapCenterCoord);
-      });
-  };
-  googleMap_initialize();
 
 });
 
