88df2291635a14f9dc7799db3ba5b672e3d64a4b
Serhiy <serhiyhulyi93@gmail.com>
2016-02-21 15:10:40 +0200

    something more

--- js/main.js
+++ js/main.js
@@ -32,6 +32,37 @@ jQuery(document).ready(function($) {
     mainClass: 'my-mfp-slide-bottom'
   });
 
+  $('.photo_gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-with-zoom',
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+    modal: false,
+
+    closeBtnInside: true,
+    preloader: false,
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1], // Will preload 0 - before current, and 1 after the current image
+      tCounter: '<strong>%curr% / %total%</strong>' // Markup for "1 of 7" counter
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
   $(document).on('click', '.popup-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
